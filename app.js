414155d5f1482b80e2ef1a54bdf7516b2214e0c9
MysteroCode <115699352+PhoenixGoIt@users.noreply.github.com>
2023-07-30 15:24:01 +0300

    fix

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter,)
-
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
 })
--- routes/api/contactsRoutes.js
+++ routes/api/contactsRoutes.js
@@ -1,17 +1,20 @@
 import express from 'express'
 import ctrl from '../../controllers/contacts.js'
 import isValidId from '../../middlewares/isValidId.js'
+import validateBody from '../../decorators/validateBody.js'
+import { addContactShema, addUpdateFavotiteShema } from '../../shema/contacts-shema.js'
+import isEmptyBody from '../../middlewares/isEmptyBody.js'
 const router = express.Router()
 
 router.get('/',  ctrl.getAll)
 
 router.get('/:contactId', isValidId, ctrl.getById)
 
-router.post('/', ctrl.add)
+router.post('/', isEmptyBody, validateBody(addContactShema), ctrl.add)
 
 router.delete('/:contactId',isValidId, ctrl.del)
 
-router.put('/:contactId',isValidId, ctrl.updateById)
+router.put('/:contactId',isEmptyBody, validateBody(addContactShema),isValidId, ctrl.updateById)
 
-router.patch('/:contactId/favorite',isValidId, ctrl.patchById)
+router.patch('/:contactId/favorite',isEmptyBody, validateBody(addUpdateFavotiteShema),isValidId, ctrl.patchById)
 export default router
