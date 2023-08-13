d89e9f6b516f936e44f8f8ae6385d6baed490383
MysteroCode <115699352+PhoenixGoIt@users.noreply.github.com>
2023-08-13 15:33:51 +0300

    add

--- shema/user-shema.js
+++ shema/user-shema.js
@@ -7,7 +7,3 @@ export const loginShema = Joi.object({
         email: Joi.string().email().required(),
         password: Joi.string().min(6).required(),
 })
-
-export const addUpdateAvatarShema = Joi.object({
-        avatar: Joi.boolean().required(),
-    })
